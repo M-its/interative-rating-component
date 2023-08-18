ee3ad52e54c401e56b36e40c7b40fccbda90cf83
Mitsrael Souza <mitarael9@gmail.com>
2023-08-18 18:07:25 -0300

    Add new displayModal() the code is almost done now

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search)
+let rating = urlParams.get("rating")
 const radioSpan = document.querySelectorAll("span.rating")
+const form = document.querySelector("form#rating-form")
+const modal = document.querySelector(".modal")
+const givenRate = document.querySelector("span#given-rate")
 
-function getRadioInputValue() {
-    rating = urlParams.get("rating")
-    console.log(rating)
-}
-
-function displayModal() {}
+console.log(rating)
 
 function chageRadioSpanBackground() {
     radioSpan.forEach((element) => {
@@ -19,9 +18,30 @@ function chageRadioSpanBackground() {
             // Apply the styles to the clicked element
             element.style.backgroundColor = "hsl(216, 12%, 54%)"
             element.style.color = "hsl(0, 0%, 100%)"
+            rating = element.textContent
         })
     })
 }
 
-getRadioInputValue()
+function displayModal() {
+    if (rating !== null) {
+        form.addEventListener("submit", (event) => {
+            event.preventDefault()
+            form.style.display = "none"
+            givenRate.innerHTML = ` ${rating} `
+            modal.style.display = "flex"
+        })
+    } else {
+        console.error("Please select a valid rating")
+    }
+}
+
+function resetRatingQueryParam() {
+    const url = new URL(window.location.href)
+    url.searchParams.delete("rating")
+    window.history.replaceState(null, null, url)
+}
+
 chageRadioSpanBackground()
+displayModal()
+resetRatingQueryParam()
