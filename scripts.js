05082fd5a344a72718478b1a47ba611e59edb762
Mitsrael Souza <mitarael9@gmail.com>
2023-08-19 16:48:52 -0300

    Fixed the modal behavior

--- scripts.js
+++ scripts.js
@@ -5,8 +5,6 @@ const form = document.querySelector("form#rating-form")
 const modal = document.querySelector(".modal")
 const givenRate = document.querySelector("span#given-rate")
 
-console.log(rating)
-
 function chageRadioSpanBackground() {
     radioSpan.forEach((element) => {
         element.addEventListener("click", () => {
@@ -24,16 +22,16 @@ function chageRadioSpanBackground() {
 }
 
 function displayModal() {
-    if (rating !== null) {
-        form.addEventListener("submit", (event) => {
-            event.preventDefault()
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        if (rating !== null) {
             form.style.display = "none"
             givenRate.innerHTML = ` ${rating} `
             modal.style.display = "flex"
-        })
-    } else {
-        console.error("Please select a valid rating")
-    }
+        } else {
+            alert("Please select a valid rating")
+        }
+    })
 }
 
 function resetRatingQueryParam() {
